57b93f6af677cb265b22e364331d4c39db2da6fc
Narsi <narsi.sanda@gmail.com>
2021-03-22 09:06:42 +0530

    empty the repo

--- vehicle-fleet-service/src/main/java/com/fmp/shared/APIConstants.java
+++
@@ -1,8 +0,0 @@
-package com.fmp.shared;
-
-public interface APIConstants {
-
-	String DRIVER_API_BASE_URL = "http://localhost:7076/api/drivers/";
-	//http://localhost:7075/api/vehicles
-	String VEHICLE_API_BASE_URL = "http://localhost:7075/api/vehicles/";
-}
